9685e713e220ab745e0fc8e93ba86a3378ced981
Skripunov David <skripunovd333@gmail.com>
2025-03-21 16:12:51 +0300

    Add files via upload

--- FileName.c
+++ FileName.c
@@ -1,39 +1,381 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <locale.h>
-
+#include <math.h>
 #include <stdio.h>
-void date()
-{
-	puts("   __      __      __   __ ");
-	puts("/|  /  /|   _|    |  | |__ ");
-	puts(" |  |   |  |__    |__| |__|");
-	getchar();
-	return 0;
-}
-void figures()
-{
-	setlocale(LC_CTYPE, "RUS");
-	puts("             *     ____                         ");
-	puts("            **    /    \                        ");
-	puts("          _**    /\     \                       ");
-	puts("         /__/|  /  \     \                      ");
-	puts("         |  || /    \     \                     ");
-	puts("         |  ||/      \    /|                    ");
-	puts("         |  |/        \  / |                    ");
-	puts("         |  /          \/ /|                    ");
-	puts("         *************** | |                    ");
-	puts("         *         __  * |/|                    ");
-	puts("         *  ___   |  | *   |                    ");
-	puts("         *  | |   |__| *   |                    ");
-	puts("         *  | |        *  /                     ");
-	puts("         ****************/                      ");
-	getchar();
-	return 1;
+#include <stdlib.h>
+#include <time.h>
+
+#define PI 3.14 // число пи
+#define n 10 // размер массива
+#define SCREENW 120 // ширина графика
+#define SCREENH 30 // высота графика
+
+
+double F1(double x);  // определение функции F1
+double F2(double x);  // определение функции F2
+
+double integrand(double t, double d);
+
+struct znachenia {
+    double x; //значение х 
+    double* values;
+    int count;// количество х 
+    double* array;
+    double xmin;
+    double xmax;
+    double dx;
+};
+
+struct znachenia* A(double xmin, double dx, double* values);  // генерирует массив значений x
+struct znachenia* B(double* array, int N, double xmin, double xmax);  // генерирует уникальные случайные значения
+void result(FILE* outputFile, double x, int func_num, double (*func)(double));  // выводит результат в файл
+double find_min(double (*func)(double), double* values, int N);  // находит минимальное значение
+double find_max(double (*func)(double), double* values, int N);  // находит максимальное значение
+double differentiate(double (*func)(double), double x, double epsilon);  // приближенное дифференцирование
+typedef double (*TFunc)(double);
+void plot(double x0, double x1, TFunc f);
+// Прототип функции для табуляции функции на заданном интервале
+void tabulate(double (*func)(double), double xn, double xk, double h);
+
+
+
+void menu(FILE* outputFile);  // меню для выбора действий
+
+
+struct znachenia* mode1(const char* filename, struct znachenia* data);
+
+
+int main() {
+
+    setlocale(LC_CTYPE, "RUS");  // установка локали для русского языка
+    FILE* outputFile = fopen("output.txt", "w");  // открытие файла для записи
+    menu(outputFile);  // вызов меню
+    fclose(outputFile);  // закрытие файла
+    return 0;
 
 }
-void main()
-{
-	setlocale(LC_CTYPE, "RUS");
-	
-	date();
-	figures();
+
+
+void menu(FILE* outputFile) {
+    int choice;
+    struct znachenia x_for_formuls; // структура используемая для инициализации числа х 
+    double  xn, xk, h, d, t;
+    double values[n], xmin, xmax, dx, epsilon = 1e-5;
+    int func_choice;
+    double* array;
+
+    while (1) {
+        printf("Главное меню:\n");
+        printf("1. Вычисление функции\n");
+        printf("2. Табуляция функции\n");
+        printf("3. Генерация уникальных случайных значений\n");
+        printf("4. Определение минимального и максимального значений\n");
+        printf("5. Дифференцирование функции в заданной точке\n");
+        printf("6. График функиции f1\n");
+        printf("7. График функции f2\n");
+        printf("8. Выход\n");
+        scanf("%d", &choice);  // выбор пункта меню
+
+        switch (choice) {
+        case 1:
+            printf("Выберите функцию:\n1 - F1\n2 - F2\n3 - integrand\n");
+            scanf("%d", &func_choice);  // выбор функции
+            printf("Введите значение x, t, d:\n");
+            scanf("%lf", &x_for_formuls.x);  // ввод значения x
+            scanf("%lf", &d);
+            scanf("%lf", &t);
+            fprintf(outputFile, "|x\t\t|F%d(x)\t\t|\n", func_choice);  // вывод в файл
+            fprintf(outputFile, "-----------------------\n");
+            switch (func_choice) {
+            case 4: result(outputFile, x_for_formuls.x, 1, F1); break;  // вычисление и вывод результата для F1
+            case 5: result(outputFile, x_for_formuls.x, 1, F2); break;  // вычисление и вывод результата для F2
+            case 1:
+                printf("%d\n", F1(x_for_formuls.x));
+                mode1("FILE.txt", &x_for_formuls);
+                break;  // вычисление и вывод результата для F1
+            case 2:
+                printf("%d\n", F2(x_for_formuls.x));
+                mode1("FILE.txt", &x_for_formuls);
+                break;  // вычисление и вывод результата для F2printf("%d", F2);  
+            case 3:
+                printf("%d\n", integrand);
+                mode1("FILE.txt", &x_for_formuls);
+                break; //вычисление и вывод рез-та для интегрального логарифма 
+
+            default: printf("Некорректный выбор функции.\n");
+            }
+            break;
+        case 2:
+            printf("Выберите функцию:\n1 - F1\n2 - F2\n");
+            scanf("%d", &func_choice);  // выбор функции
+            printf("Введите начальное значение (xmin):\n");
+            scanf("%lf", &xmin);  // ввод xmin
+            printf("Введите шаг (dx):\n");
+            scanf("%lf", &dx);  // ввод dx
+            A(xmin, dx, values);  // вызов функции A для генерации значений
+            fprintf(outputFile, "|x\t\t|F%d(x)\t\t|\n", func_choice);  // вывод в файл
+            fprintf(outputFile, "_________________________\n");
+            for (int i = 0; i < n; i++) {  // перебор значений и вычисление функции
+                switch (func_choice) {
+                case 1: fprintf(outputFile, "|%.5lf\t|%lf\t|\n", values[i], F1(values[i])); break;
+                case 2: fprintf(outputFile, "|%.5lf\t|%lf\t|\n", values[i], F2(values[i])); break;
+
+
+
+                default: printf("Некорректный выбор функции.\n"); break;
+                }
+            }
+            break;
+        case 3:
+            printf("Введите начальное значение (xmin):\n");
+            scanf("%lf", &xmin);  // ввод xmin
+            printf("Введите конечное значение (xmax):\n");
+            scanf("%lf", &xmax);  // ввод xmax
+            printf("Введите количество значений N:\n");
+            scanf("%d", &x_for_formuls.count);  // ввод N
+            array = (double*)malloc(x_for_formuls.count * sizeof(double));  // выделение памяти для массива
+            B(array, x_for_formuls.count, xmin, xmax);  // вызов функции B для генерации уникальных случайных значений
+            fprintf(outputFile, "Сгенерированные уникальные значения:\n");
+            for (int i = 0; i < x_for_formuls.count; i++) {
+                fprintf(outputFile, "%lf\n", array[i]);  // вывод значений в файл
+            }
+            free(array);  // освобождение памяти
+            break;
+        case 4:
+            printf("Выберите функцию:\n1 - F1\n2 - F2\n");
+            scanf("%d", &func_choice);  // выбор функции
+            printf("Введите начальное значение (xmin):\n");
+            scanf("%lf", &xmin);  // ввод xmin
+            printf("Введите конечное значение (xmax):\n");
+            scanf("%lf", &xmax);  // ввод xmax
+            printf("Введите количество значений N:\n");
+            scanf("%d", &x_for_formuls.count);  // ввод N
+
+            array = (double*)malloc(x_for_formuls.count * sizeof(double));  // выделение памяти для массива
+            B(array, x_for_formuls.count, xmin, xmax);  // генерация уникальных значений
+
+            double (*selected_func)(double) = NULL;  // указатель на выбранную функцию
+            switch (func_choice) {
+            case 1: selected_func = F1; break;  // выбор F1
+            case 2: selected_func = F2; break;  // выбор F2
+
+            default:
+                printf("Некорректный выбор функции.\n");
+                free(array);
+                return;
+            }
+
+            double* func_values = (double*)malloc(x_for_formuls.count * sizeof(double));  // выделение памяти для значений функции
+            if (!func_values) {
+                printf("Ошибка выделения памяти для массива значений функции.\n");
+                free(array);
+                return;
+            }
+
+            for (int i = 0; i < x_for_formuls.count; i++) {  // вычисление значений функции для каждого аргумента
+                func_values[i] = selected_func(array[i]);
+            }
+
+            double min_value = find_min(selected_func, func_values, x_for_formuls.count);  // нахождение минимального значения
+            double max_value = find_max(selected_func, func_values, x_for_formuls.count);  // нахождение максимального значения
+
+            printf("Минимальное значение: %.5lf\n", min_value);  // вывод минимального значения
+            printf("Максимальное значение: %.5lf\n", max_value);  // вывод максимального значения
+
+            free(func_values);  // освобождение памяти
+            free(array);  // освобождение памяти
+            break;
+
+
+        case 5:
+            printf("Введите точку для дифференцирования:\n");
+            scanf("%lf", &x_for_formuls.x);  // ввод точки
+            printf("Производная функции F1 в точке %.5lf: %.5lf\n", x_for_formuls.x, differentiate(F1, x_for_formuls.x, epsilon));  // вычисление производной
+            break;
+        case 8:
+            printf("Выход из программы.\n");
+            return;
+
+
+        case 7:
+            printf("Введите начальное значение графика: \n");
+            scanf("%lf", &xn);
+            printf("Введите конечное значение графика: \n");
+            scanf("%lf", &xk);
+            plot(xn, xk, F1);
+            break;
+
+
+        case 6:
+            printf("Введите начальное значение графика: \n");
+            scanf("%lf", &xn);
+            printf("Введите конечное значение графика: \n");
+            scanf("%lf", &xk);
+            plot(xn, xk, F2);
+            break;
+        default:
+            printf("Некорректный выбор. Попробуйте снова.\n");
+            break;
+        }
+    }
+}
+
+// Функция F1
+double F1(double x) {
+    if (x == 0 || x == PI) {
+        return NAN;
+    }
+    return (-log(x / PI)) * cos(0.7 * x);
+}
+
+// Функция F2
+double F2(double x) {
+    if (x > -1) {
+        return x + 1;
+    }
+    else if (-2 <= x && x <= -1) {
+        return 1 - pow(x, 5);
+    }
+    else {
+        return x + log(fabs(cos(x)));
+    }
+}
+double integrand(double t, double d) {
+    return (d * t) / log(t);
+}
+
+
+// Функция построения графика
+void plot(double x0, double x1, TFunc f) {
+    char screen[SCREENH][SCREENW];
+    double x, y[SCREENW];
+    double ymin = 0, ymax = 0;
+    double hx, hy;
+    int i, j;
+    int xz, yz;
+
+    hx = (x1 - x0) / (SCREENW - 1);
+
+    // Расчет значений для каждой точки
+    for (i = 0, x = x0; i < SCREENW; ++i, x += hx) {
+        y[i] = f(x);
+        if (y[i] < ymin) {
+            ymin = y[i];
+        }
+        if (y[i] > ymax) {
+            ymax = y[i];
+        }
+    }
+
+    hy = (ymax - ymin) / (SCREENH - 1);
+    yz = (int)floor((0.0 - ymin) / hy + 0.5);
+    xz = (int)floor((0.0 - x0) / hx + 0.5);
+
+    // Инициализация массива пробелами
+    for (j = 0; j < SCREENH; ++j) {
+        for (i = 0; i < SCREENW; ++i) {
+            screen[j][i] = ' ';
+        }
+    }
+
+    // Отображение осей
+    for (j = 0; j < SCREENH; ++j) {
+        for (i = 0; i < SCREENW; ++i) {
+            if (j == yz && i == xz) screen[j][i] = '+';
+            else if (j == yz) screen[j][i] = '-';
+            else if (i == xz) screen[j][i] = '|';
+        }
+    }
+
+    // Определение положения значений 
+    for (i = 0; i < SCREENW; ++i) {
+        j = (int)floor((ymax - y[i]) / hy + 0.5);
+        if (j >= 0 && j < SCREENH) screen[j][i] = '*';
+    }
+
+    // Печать массива символов
+    for (j = 0; j < SCREENH; ++j) {
+        for (i = 0; i < SCREENW; ++i) {
+            putchar(screen[j][i]);
+        }
+        putchar('\n');
+    }
+}
+
+
+
+
+
+
+struct znachenia* A(double xmin, double dx, double* values) {
+    struct znachenia data;
+
+    data.x = xmin;
+    data.dx = dx;
+    data.values = values;
+
+    for (int i = 0; i < n; i++)
+    {
+        values[i] = xmin + i * dx;  // генерация массива значений с шагом dx
+
+    }
+
+    return &data;
+}
+struct znachenia* B(double* array, int N, double xmin, double xmax) {
+    struct znachenia data;
+    int count = 0;
+    while (count < N) {
+        double num = xmin + (double)rand() / RAND_MAX * (xmax - xmin);  // генерация случайного числа
+        int unique = 1;
+        for (int i = 0; i < count; i++) {
+            if (fabs(array[i] - num) < 1e-6) {  // проверка уникальности значения
+                unique = 0;
+                break;
+            }
+        }
+        if (unique) array[count++] = num;  // добавление уникального значения в массив
+    }
+    data.xmax = xmax;
+    data.array = array;
+    //data.count = count;
+    data.xmin = xmin;
+    data.count = count;
+    return &data;
+
+}
+void result(FILE* outputFile, double x, int func_num, double (*func)(double)) {
+    fprintf(outputFile, "|%.3lf\t|%lf\t|\n", x, func(x));  // вывод результата в файл
+}
+double find_min(double (*func)(double), double* values, int N) {
+    double min = values[0];
+    for (int i = 1; i < N; i++)
+        if (values[i] < min)  // поиск минимального значения
+            min = values[i];
+    return min;
+}
+double find_max(double (*func)(double), double* values, int N) {
+    double max = values[0];
+    for (int i = 1; i < N; i++)
+        if (values[i] > max)  // поиск максимального значения
+            max = values[i];
+    return max;
+}
+double differentiate(double (*func)(double), double x, double epsilon) {
+    return (func(x + epsilon) - func(x)) / epsilon;  // приближенное дифференцирование
+}
+
+struct znachenia* mode1(const char* filename, struct znachenia* data) {
+    FILE* file = fopen(filename, "w");
+    if (!file) {
+        perror("Ошибка открытия файла");
+        return NULL;
+    }
+    //for (int i = 0; i < size; i++) {
+    fprintf(file, "%lf %lf\n", data->x, data->count);
+
+    fclose(file);
+    printf("Данные записаны в файл %s\n", filename);
+    return &data;
 }
